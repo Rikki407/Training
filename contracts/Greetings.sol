bad13321a6714a6186c535ae19dbd6e03da17288
Rikki407 <rishablamba407@gmail.com>
2018-03-08 17:57:25 +0530

    added migration for Greetings.sol

--- contracts/Greetings.sol
+++ contracts/Greetings.sol
@@ -4,6 +4,13 @@ contract Greetings{
     string message;
 
     function Greetings() public{
-        message = "Hello World!!!"
+        message = "Hello World!!!";
     }
+    function setMessage(string _message) public{
+        message = _message;
+    }
+    function getMessage() public view returns(string){
+        return message;
+    }
+
 }
